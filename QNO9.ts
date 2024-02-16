39d3dad45718dab955e5426d96e1f2bc4cce4b5a
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 00:30:40 +0500

    Add files via upload

---
+++ QNO9.ts
@@ -0,0 +1,5 @@
+// //  QNO:9
+// // Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+// let aname:number=99;
+// console.log(` This is my  most Favourite number ${aname} `)
